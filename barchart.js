0a48a8e0a83076759110c99c6488311ce9ed99bf
MarquezFelipe <105685202+MarquezFelipe@users.noreply.github.com>
2024-09-29 22:25:04 -0300

    feat: datos canciones paulo

--- barchart.js
+++ barchart.js
@@ -9,17 +9,17 @@ document.body.appendChild(nuevoDiv);
 
 
 var trace1 = {
-    x: ['A', 'B', 'C', 'D', 'E'],
-    y: [20, 14, 23, 25, 22],
+    x: ["Adan y Eva", "Tal Vez", "Nena Maldición (feat. Lenny Tavárez)", "Cuando Te Besé", "Te Amo", "Chica Paranormal", "Por Eso Vine", "Plan A"],
+    y: [951749705, 783042405, 645784654	, 608552565, 498785803, 376295353, 311221308, 290947051],
     type: 'bar'
 };
 
 var data = [trace1];
 
 var layout = {
-    title: 'Bar Chart Example',
-    xaxis: { title: 'Categories' },
-    yaxis: { title: 'Values' }
+    title: 'Reproducciones de las canciones más populares de Paulo Londra en Spotify',
+    xaxis: { title: 'Canciones' },
+    yaxis: { title: 'Reproducciones' }
 };
 
 Plotly.newPlot('barchart 1', data, layout);
--- main.js
+++ main.js
@@ -1,2 +1,1 @@
 import './barchart.js';
-import './bubblechart.js';
